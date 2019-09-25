cb981121e1ce4f2b80b7816a981d23b6606589a6
Pierre Sid-idris <pierre.sididris@live.fr>
2019-09-25 12:37:03 +0200

    get parameter

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get('/', function (req, res) {
 	  res.send('Hello World!')
 })
 
+app.get('/hello', function (req, res) {
+	if(req.query.nom==null){
+		res.send('Quel est votre nom !')
+	}
+	else {
+		res.send('Bonjour ' + req.query.nom + ' !')
+	}
+
+
+})
+
 app.listen(process.env.PORT || 3000, function () {
 	console.log("server.js listening on port 3000!");
 })
